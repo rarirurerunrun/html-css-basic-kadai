2b27b67729c1cae9ac20f6e1f3e2005ca69111c3
rarirurerunrun <140226852+rarirurerunrun@users.noreply.github.com>
2023-09-13 23:07:55 +0900

    kadai005をアップロード

---
+++ kadai005/bmi.js
@@ -0,0 +1,13 @@
+//変数の宣言
+let bodyWeight;
+let heiGht;
+//値の代入
+bodyWeight = 68;
+heiGht = 1.7;
+//コンソールの出力
+console.log(heiGht*heiGht);
+console.log(68);
+//値の再代入
+heiGht = 2.89;
+//コンソールへの出力
+console.log(bodyWeight/heiGht);
